12f70c4ecf3354312611661816dce84a56317a4d
Divya sree Kondapalli <divya.k@feuji.com>
2025-08-13 18:35:43 +0530

    added selected box

--- app.py
+++ app.py
@@ -1,2 +1,12 @@
 import streamlit as st
+import pandas as pd
 st.title('deploy new app')
+df=pd.read_csv('deploy_app\data\titani_data.csv')
+df['Embarked']=df['Embarked'].fillna('Unknown')
+embarked_port=list(df['Embarked'].unique())
+sex_port=list(df['sex'].unique())
+
+
+col1,col2=st.columns(2)
+selected_port=col1.selectbox(label='selected_port',options=embarked_port)
+selected_gender=col2.selectbox('selected_geander',options=sex_port)
